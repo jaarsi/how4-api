fd0a33e851f9f524edbcf2129f84c2c3351da953
Marco Locks <marcolocks@gmail.com>
2020-10-18 21:15:32 -0300

    ajustes após revisão equipe

--- docs/arquivos/mercearia-univali.sql
+++ docs/arquivos/mercearia-univali.sql
@@ -13,6 +13,24 @@ CREATE TABLE `tbCliente` (
   `stInativo` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Situação 0 - ativo/1 - inativo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `tbProduto` (
+  `idProduto` int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `noProduto` varchar(250) NOT NULL COMMENT 'Nome do produto',
+  `deProduto` varchar(500) NULL COMMENT 'Descrição do produto',
+ -- `tpProduto` varchar(500) NULL COMMENT 'Tipo do produto/categoria',
+  `vrUnitario` decimal(10,2) NOT NULL COMMENT 'Valor unitário do produto',
+  `dtCadastro` datetime NOT NULL COMMENT 'Data em que o produto foi cadastrado',
+  `stInativo` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Situação 0 - ativo/1 - inativo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tbEstoque` (
+  `idEstoque` int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `idProduto` int(6) UNSIGNED NOT NULL COMMENT 'FK do produto',
+  `qtProduto` int(6) NOT NULL COMMENT 'Quantidade disponivel do produto',
+  `stInativo` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Situação 0 - ativo/1 - inativo',
+  CONSTRAINT `FK_ProdutoEstoque` FOREIGN KEY (idProduto) REFERENCES tbProduto(idProduto)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `tbPedido` (
   `idPedido` int(7) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `idCliente` int(6) UNSIGNED NOT NULL COMMENT 'FK Cliente do pedido',
@@ -22,32 +40,15 @@ CREATE TABLE `tbPedido` (
   CONSTRAINT `FK_ClientePedido` FOREIGN KEY (idCliente) REFERENCES tbCliente(idCliente)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `tbProduto` (
-  `idProduto` int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `noProduto` varchar(250) NOT NULL COMMENT 'Nome do produto',
-  `deProduto` varchar(500) NULL COMMENT 'Descrição do produto',
- -- `tpProduto` varchar(500) NULL COMMENT 'Tipo do produto/categoria',
-  `vrUnitario` decimal(15,2) NOT NULL COMMENT 'Valor unitário do produto',
-  `dtCadastro` datetime NOT NULL COMMENT 'Data em que o produto foi cadastrado',
-  `stInativo` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Situação 0 - ativo/1 - inativo'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE `tbPedidoItem` (
   `idPedidoItem` int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `idPedido` int(6) UNSIGNED NOT NULL COMMENT 'FK do pedido',
   `idProduto` int(6) UNSIGNED NOT NULL COMMENT 'FK do produto',
   `nuOrdem` int(3) UNSIGNED NOT NULL COMMENT 'Ordem do produto',
-  `qtProdutoPedido` int(6) NOT NULL COMMENT 'Quantidade solicitada no pedido',
-  CONSTRAINT `FK_PedidoProdutoPedido` FOREIGN KEY (idPedido) REFERENCES tbPedido(idPedido),
-  CONSTRAINT `FK_PedidoProdutoProduto` FOREIGN KEY (idProduto) REFERENCES tbProduto(idProduto)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `tbEstoque` (
-  `idEstoque` int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `idProduto` int(6) UNSIGNED NOT NULL COMMENT 'FK do produto',
-  `qtProduto` int(6) NOT NULL COMMENT 'Quantidade disponivel do produto',
-  `stInativo` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Situação 0 - ativo/1 - inativo',
-  CONSTRAINT `FK_ProdutoEstoque` FOREIGN KEY (idProduto) REFERENCES tbProduto(idProduto)
+  `qtProdutoItem` int(6) NOT NULL COMMENT 'Quantidade solicitada no pedido',
+  `vrUnitario` decimal(10,2) NOT NULL COMMENT 'Valor unitário do item do pedido',
+  CONSTRAINT `FK_PedidoPedidoItem` FOREIGN KEY (idPedido) REFERENCES tbPedido(idPedido),
+  CONSTRAINT `FK_ProdutoPedidoItem` FOREIGN KEY (idProduto) REFERENCES tbProduto(idProduto)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Inserindo clientes
@@ -81,13 +82,13 @@ insert into db_Mercearia_DES.tbPedido(idCliente,dtPedido,vrTotal) values (3,sysd
 insert into db_Mercearia_DES.tbPedido(idCliente,dtPedido,vrTotal) values (4,sysdate(),7.5); -- 5 pães, 1 queijo, 1 mortadela
 
 -- Inserindo Itens do Pedido
-insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoPedido) values (1,1,1,2); -- pedido 1 João, Item 1: 2 pães
-insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoPedido) values (1,2,2,1); -- pedido 1 João, Item 2: 1 coca
-insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoPedido) values (2,1,1,10); -- pedido 2 Maria, Item 1: 10 pães
-insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoPedido) values (3,3,1,4); -- pedido 3 Joana, Item 1: 4 chineques
-insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoPedido) values (4,1,1,5); -- pedido 4 José Item 1: 5 pães
-insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoPedido) values (4,5,2,1); -- pedido 4 José Item 2: 1 queijo
-insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoPedido) values (4,6,3,1); -- pedido 4 José Item 3: 1 mortadela
+insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoItem,vrUnitario) values (1,1,1,2,.5); -- pedido 1 João, Item 1: 2 pães
+insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoItem,vrUnitario) values (1,2,2,1,5.50); -- pedido 1 João, Item 2: 1 coca
+insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoItem,vrUnitario) values (2,1,1,10,.5); -- pedido 2 Maria, Item 1: 10 pães
+insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoItem,vrUnitario) values (3,3,1,4,.75); -- pedido 3 Joana, Item 1: 4 chineques
+insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoItem,vrUnitario) values (4,1,1,5,.5); -- pedido 4 José Item 1: 5 pães
+insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoItem,vrUnitario) values (4,5,2,1,2.5); -- pedido 4 José Item 2: 1 queijo
+insert into db_Mercearia_DES.tbPedidoItem(idPedido,idProduto,nuOrdem,qtProdutoItem,vrUnitario) values (4,6,3,1,2.5); -- pedido 4 José Item 3: 1 mortadela
 
 commit;
 
@@ -97,13 +98,14 @@ commit;
 ---------------
 
 -- CONSULTAR PEDIDO/ITENS DO PEDIDO
-select ped.dtPedido as dataPedido,
+select ped.dtPedido as dataPedido,		
        ped.idPedido as numeroPedido,
+       cli.noCliente,
        pdi.nuOrdem as itemPedido,
        pro.noProduto as nomeProduto,
-       pdi.qtProdutoPedido as quantidade,
-       pro.vrUnitario,
-       pdi.qtProdutoPedido * pro.vrUnitario as vrTotalItem
+       pdi.qtProdutoItem as quantidade,
+       pdi.vrUnitario,
+       pdi.qtProdutoItem * pro.vrUnitario as vrTotalItem
 from tbProduto pro
 inner join tbPedidoItem pdi on pdi.idProduto = pro.idProduto
 inner join tbPedido ped on ped.idPedido = pdi.idPedido
